d60df400c542fa928ceeb7985ca6c3ae6155d467
Deokant Kumar <deokant.kumar2025@glbajajgroup.org>
2026-06-28 11:26:35 +0530

    day_21

---
+++ day_21/Q83_count_vowels_&_consonants_in_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int vowels = 0, consonants = 0;
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);  
+
+    while (str[i] != '\0') 
+    {
+        char ch = str[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) 
+        {
+            if (ch >= 'A' && ch <= 'Z') 
+            {
+                ch = ch + 32;
+            }
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else 
+            {
+                consonants++;
+            }
+        }
+        i++;
+    }
+
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+
+    return 0;
+}
